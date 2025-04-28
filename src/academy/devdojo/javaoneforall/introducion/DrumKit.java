7b994ee1749c1b4aa66aaa1e8d7930624b203e33
Allan <allanresnauer1@gmail.com>
2025-04-28 18:09:45 -0300

    Novo exercício resolvido

---
+++ src/academy/devdojo/javaoneforall/introducion/DrumKit.java
@@ -0,0 +1,32 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class DrumKit {
+        boolean topHat = true;
+        boolean snare = true;
+
+        void playTopHat() {
+            System.out.println("ding ding da-ding");
+        }
+
+        void playSnare() {
+            System.out.println("bang bang ba-bang");
+        }
+    }
+
+    class DrumKitTestDrive {
+        public static void main(String[] args) {
+            DrumKit d = new DrumKit();
+
+            if (d.snare == true) {
+                d.playSnare();
+            }
+
+            d.snare = false;
+
+            d.playTopHat();
+        }
+    }
+
+
+
+
